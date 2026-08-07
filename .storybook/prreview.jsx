a2556adb28861f6730f4ac045632f04d4c55889c
vansh21827 <vansh21827@gmail.com>
2026-08-07 16:20:23 +0530

    Add Storybook preview configuration with decorators

---
+++ .storybook/prreview.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter } from "react-router-dom";
+import "../src/styles/global.css";
+
+/** @type {import('@storybook/react-vite').Preview} */
+const preview = {
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+
+  parameters: {
+    controls: {
+      expanded: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    actions: {
+      argTypesRegex: "^on[A-Z].*",
+    },
+
+    layout: "centered",
+
+    a11y: {
+      test: "todo",
+    },
+  },
+};
+
+export default preview;
